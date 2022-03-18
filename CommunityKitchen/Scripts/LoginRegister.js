7cedcaf1d579c92b38b4b0107089be13a6b1497c
GiannisMi <giannismichailidis1234@hotmail.com>
2022-03-18 17:31:43 +0200

    Add Script MetaLogin setup facebook login

---
+++ CommunityKitchen/Scripts/LoginRegister.js
@@ -0,0 +1,13 @@
+﻿function signupToggle() {
+    var container = document.querySelector('.container23');
+    container.classList.toggle('active');
+    var popup = document.querySelector('.signup-form');
+    popup.classList.toggle('active');
+}
+function loginToggle() {
+    var container = document.querySelector('.container23');
+    container.classList.toggle('active');
+    var popup = document.querySelector('.login-form');
+    popup.classList.toggle('active');
+
+}
