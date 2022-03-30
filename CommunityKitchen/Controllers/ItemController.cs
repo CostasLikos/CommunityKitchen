ccb446b149bdb42a36db563f5ce2f089ed05e121
Costas Likos <costaslikos@gmail.com>
2022-03-30 13:54:14 +0300

    item

---
+++ CommunityKitchen/Controllers/ItemController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Entities;
+using MyDataBase;
+
+namespace CommunityKitchen.Controllers
+{
+    public class ItemController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Item
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Items.ToListAsync());
+        }
+
+        // GET: Item/Details/5
+        public async Task<ActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = await db.Items.FindAsync(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // GET: Item/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Item/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,ItemName,Quantity,Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                item.Id = Guid.NewGuid();
+                db.Items.Add(item);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        // GET: Item/Edit/5
+        public async Task<ActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = await db.Items.FindAsync(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: Item/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,ItemName,Quantity,Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        // GET: Item/Delete/5
+        public async Task<ActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = await db.Items.FindAsync(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: Item/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            Item item = await db.Items.FindAsync(id);
+            db.Items.Remove(item);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
