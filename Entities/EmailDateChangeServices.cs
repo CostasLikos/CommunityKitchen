f00c1af8a361bafc6529ce74787528b5e5e1e235
Dstrats <james_strats@hotmail.com>
2022-04-17 18:01:34 +0300

    AlmostThere

--- Entities/EmailDateChangeServices.cs
+++ Entities/EmailDateChangeServices.cs
@@ -10,8 +10,9 @@ namespace Entities
 {
     public static class EmailDateChangeServices
     {
-        public static void EventDateChangedEmail(string userEmail, string eventName,DateTime date)
+        public static void EventDateChangedEmail(string userEmail, string eventName,DateTime date, string name)
         {
+            var userName = string.IsNullOrEmpty(name.Trim())?"Sir/Madam":name;
             try
             {
                 using (var mail = new MailMessage())
@@ -23,7 +24,11 @@ namespace Entities
                     mail.Subject = $"Date of event {eventName} Changed to {date.ToShortDateString()}";
                     mail.SubjectEncoding = System.Text.Encoding.UTF8;
 
-                    mail.Body = $"<div>datechange email: Irthe to mail Malaka</div>";
+                    mail.Body = $"  <div><h4>Dear {userName},</h4><br/>" +
+                        $"<p> We would like to inform you that your pennis has shrunk by 11 inches, please contact us for CHEAP pennis enlargemt methods.</p>" +
+                        $"<p> Please visit us on {eventName} at the new date for further enlargement {date}</p>" +
+                        "<p> Also the date  of your event has cahnged</p><p> With regards,</p><p> Money makes your pick bigger</p></div>";
+
                     mail.IsBodyHtml = true;
                     mail.BodyEncoding = System.Text.Encoding.UTF8;
 
@@ -42,6 +47,46 @@ namespace Entities
                 Console.Write(e.Message);
                 throw;
             }
+
+        }
+        public static void NewEventMail(string userEmail, string eventName, DateTime date, string name)
+        {
+            var userName = string.IsNullOrEmpty(name.Trim()) ? "Sir/Madam" : name;
+            try
+            {
+                using (var mail = new MailMessage())
+                {
+                    mail.From = new MailAddress("DateChangedInfo@gmail.com");
+                    mail.To.Add(userEmail);
+
+
+                    mail.Subject = $"Date of event {eventName} Changed to {date.ToShortDateString()}";
+                    mail.SubjectEncoding = System.Text.Encoding.UTF8;
+
+                    mail.Body = $"  <div><h4>Dear {userName},</h4><br/>" +
+                        $"<p> We would like to inform you that a new event has been added.</p>" +
+                        $"<p> Event {eventName} will be held on {date}, please follow if you like to assist!!!! </p>" +
+                        "<p> With regards,</p><p> Money makes your pick bigger</p></div>";
+
+                    mail.IsBodyHtml = true;
+                    mail.BodyEncoding = System.Text.Encoding.UTF8;
+
+                    using (var smtp = new SmtpClient("smtp.gmail.com"))
+                    {
+                        smtp.UseDefaultCredentials = false;
+
+                        smtp.Credentials = new NetworkCredential("DateChangedInfo@gmail.com", "321SAdmin!");
+                        smtp.EnableSsl = true;
+                        smtp.Send(mail);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                throw;
+            }
+
         }
     }
 }
