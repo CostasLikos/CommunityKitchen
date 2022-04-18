2e177ad72e22f32fea6e2fa54ba787bbb6a00bde
Niki Theof <93220601+NikiTheofilaktidou@users.noreply.github.com>
2022-04-18 15:20:38 +0300

    ABoutUs+allagh sto navbar

--- Entities/Cause.cs
+++ Entities/Cause.cs
@@ -20,6 +20,7 @@ namespace Entities
         [Required, StringLength(100)]
         public string Title { get; set ; }
         public string Description { get ; set ; }
+        public string FullDescription { get ; set ; }
         public string Photo { get; set; }
         //Additional Properties
         [Range(1, 1000000), DataType(DataType.Currency)]
